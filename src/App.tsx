87690b85ed6254e834a43c2818d485382a95ca1d
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-05-27 10:09:36 -0300

    STYLE: init sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { useBearStore } from "./store";
-
 export default function App() {
-  const bears = useBearStore((state) => state.bears);
-  const increasePopulation = useBearStore((state) => state.increasePopulation);
-
   return (
-    <div>
+    <main className="container">
       <h1>hello world</h1>
-      <h2>{bears}</h2>
-      <button onClick={increasePopulation} />
-    </div>
+    </main>
   );
 }
