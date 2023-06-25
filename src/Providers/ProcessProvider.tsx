cbbb6c5e43969085c5d8bf31411319378ce5a159
MRSS <mateusyasd@hotmail.com>
2023-06-24 22:38:10 -0300

    added memoryMap and diskTable variables

--- src/Providers/ProcessProvider.tsx
+++ src/Providers/ProcessProvider.tsx
@@ -12,7 +12,11 @@ interface ContextInterface {
     escalonamento: string,
     setEscalonamento: Function,
     processData: Array<Array<any>>,
-    setProcessData: Function
+    setProcessData: Function,
+    memoryMap: Array<number>,
+    setMemoryMap: Function,
+    diskTable: Array<number>,
+    setDiskTable: Function,
   }
 
 export const ProcessProvider = createContext<ContextInterface>({ 
@@ -21,11 +25,15 @@ export const ProcessProvider = createContext<ContextInterface>({
       numeroDeProcessos: 0, 
       setNumeroDeProcessos: (numeroDeProcessos: 0) => {}, 
       sobrecarga: 0,
-      setSobrecarga:  (sobrecarga: 0) => {}, 
+      setSobrecarga: (sobrecarga: 0) => {}, 
       quantum: 0,
-      setQuantum:  (quantum: 0) => {},
+      setQuantum: (quantum: 0) => {},
       escalonamento: "",
-      setEscalonamento:  (escalonamento: "") => {},
+      setEscalonamento: (escalonamento: "") => {},
       processData: [],
-      setProcessData:  (escalonamento: "") => {},
+      setProcessData: (processData: []) => {},
+      memoryMap: [],
+      setMemoryMap:  (memoryMap: []) => {},
+      diskTable: [],
+      setDiskTable: (diskTable: []) => {},
 })
