448a7e2c54dddf678a8def8398533c000c5e2ea6
Ricardo Adorno <ricardo.castrorc1998@gmail.com>
2023-07-03 03:03:18 -0300

    memoria, disco 100%, falta consertar a ram

--- src/components/Memory/Memory.tsx
+++ src/components/Memory/Memory.tsx
@@ -1,47 +1,184 @@
+import { useContext, useEffect, useState } from "react";
 import "./style.scss";
+import { ProcessProvider } from "../../Providers/ProcessProvider";
 
-const cellListRam = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50,
-];
+const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
 
-const cellListDisc = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
-  61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
-];
+interface IPage {
+  id: number;
+  size: number;
+  color: string;
+  state: string;
+}
+
+function RenderTable(size: number, pages: IPage[]) {
+  let cells = [];
+
+  const { cellsPages, counter } = renderPages(pages);
+
+  cells.push(cellsPages);
+
+  for (let i = counter; i < size; i++) {
+    cells.push(
+      <div className="cell">
+        <span>{i}</span>
+      </div>
+    );
+  }
+
+  return cells;
+}
+
+function renderPages(pages: IPage[]) {
+  const cellsPages = [] as any;
+
+  let counter = 1;
+
+  pages.forEach((page) => {
+    for (let i = 0; i < page.size; i++) {
+      cellsPages.push(
+        <div className="cell">
+          <span>{counter++}</span>
+          <div className="fill" style={{ backgroundColor: page.color }}></div>
+        </div>
+      );
+    }
+  });
+
+  return {
+    cellsPages,
+    counter,
+  };
+}
+
+class Ram {
+  size: number;
+  RamJSX: any;
+  currentProcesses: any;
+
+  constructor(size: number) {
+    this.size = size;
+    this.RamJSX = this.render();
+    this.currentProcesses = [];
+  }
+
+  render(): any {
+    let cells = [];
+
+    for (let i = 0; i < this.size; i++) {
+      cells.push(
+        <div className="cell">
+          <span>{i + 1}</span>
+        </div>
+      );
+    }
+
+    return (this.RamJSX = cells);
+  }
+
+  insertProcess(page: any): any {
+    const current = [...this.currentProcesses, page];
+    this.currentProcesses = current;
+
+    const currentSize = current.reduce((acc: any, curr: any) => {
+      return acc + curr.size;
+    }, 0);
+
+    let cells = [] as any;
+
+    let counter = 1;
+
+    for (let i = 0; i < this.size; i++) {
+      if (i < currentSize) {
+        current.forEach((process: any) => {
+          cells.push(
+            <div className="cell">
+              <span>{counter++}</span>
+              <div
+                className="fill"
+                style={{ backgroundColor: process.color }}
+              ></div>
+            </div>
+          );
+        });
+      } else {
+        cells.push(
+          <div className="cell">
+            <span>{i + 1}</span>
+          </div>
+        );
+      }
+    }
+
+    return (this.RamJSX = cells);
+  }
+
+  removeProcess(page: any): any {
+    let cells = [];
+
+    for (let i = 0; i < this.size; i++) {
+      cells.push(
+        <div className="cell">
+          <span>{i + 1}</span>
+        </div>
+      );
+    }
+
+    return (this.RamJSX = cells);
+  }
+}
 
 export default function Memory() {
+  const ram = new Ram(32);
+  const [ramState, setRamState] = useState(ram);
+
+  const data = useContext(ProcessProvider);
+
+  const pages = data.processData.map((process, index) => {
+    if (process.state !== "a caminho") {
+      return {
+        id: index,
+        size: process.pages,
+        color: colors[index],
+        state: process.state,
+      };
+    } else {
+      return {
+        id: index,
+        size: 0,
+        color: colors[index],
+        state: process.state,
+      };
+    }
+  });
+
+  useEffect(() => {
+    pages.map((page) => {
+      ram.insertProcess(pages[page.id]);
+      setRamState(ram);
+    });
+  }, [data.processData]);
+
   return (
     <div className="memory-container">
       <div className="ram">
-        <h2>Memória RAM</h2>
-        <div className="table">
-          {cellListRam.map((cell) => {
-            return <MemoryCell key={cell} cell={cell} />;
-          })}
-        </div>
+        <h2>Memória de Ram</h2>
+        <div className="table">{ramState.RamJSX}</div>
       </div>
 
-      <div className="disc">
-        <h2>Disco</h2>
-        <div className="table long">
-          {cellListDisc.map((cell) => {
-            return <MemoryCell key={cell} cell={cell} />;
-          })}
-        </div>
+      <button
+        onClick={() => {
+          ram.insertProcess(pages[1]);
+          setRamState(ram);
+        }}
+      >
+        Teste
+      </button>
+
+      <div className="disco">
+        <h2>Memória de Disco</h2>
+        <div className="table">{RenderTable(64, pages)}</div>
       </div>
     </div>
   );
 }
-
-const MemoryCell = (props: any) => {
-  return (
-    <div className="cell">
-      <span>{props.cell}</span>
-      <div className="fill" />
-    </div>
-  );
-};
